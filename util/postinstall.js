6861a24e63b075d18b95a5546f8e005fb6344905
enuggetry <github@meyao.net>
2017-08-23 15:17:47 -0700

    add post install, copies blast utils to the app directory

---
+++ util/postinstall.js
@@ -0,0 +1,14 @@
+var appPath = require("app-root-path").path;
+var shelljs = require("shelljs");
+var modPath = ""+shelljs.pwd();
+
+console.log("blastjs postinstall",modPath);
+
+// copy blast_* utils to the app directory.
+var src = modPath+"/util/blast_*";
+var trg = appPath;
+
+//copy scripts to approot
+shelljs.cp(src,trg);
+
+console.log("postinstall done");
