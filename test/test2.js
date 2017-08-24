7905fcc0ab3002a97d886dbda4c56885feb6bb28
enuggetry <github@meyao.net>
2017-08-23 22:18:01 -0700

    blast.blast function test

---
+++ test/test2.js
@@ -0,0 +1,28 @@
+var appPath = require("app-root-path").path;
+var blast = require('blastjs');
+var fs = require('fs-extra');
+
+blast.outputString(true); //optional, provides string output instead of JSON
+
+var dbPath = './dbs/example';
+var query = '>24.6jsd2.Tut\nGGTGTTGATCATGGCTCAGGACAAACGCTGGCGGCGTGCTTAATACATGCAAGTCGAACGGGCTACCTTCGGGTAGCTAGTG'
++'\n>24.6jsd3.Tut\nATGATCATGGCTCAGATTGAACGCTGGCGGCATGCCTTACACATGCAAGTCGAACGGCAGCACGGGGAAGGGGCAACTCTTT';
+
+var options = {
+	query: query,
+	db: dbPath,
+	outfmt: 5,
+	outputDirectory: appPath+'/tmp'
+};
+
+fs.ensureDir(options.outputDirectory);
+console.log("outputDirectory",options.outputDirectory);
+
+blast.blast(options, function (err, output) {
+  if(!err){
+    console.log(output);
+  }
+});
+
+
+
