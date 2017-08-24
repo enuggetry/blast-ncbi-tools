d5c3ce5677542112c8a2e3c644594ca7d10c20e3
enuggetry <github@meyao.net>
2017-08-24 01:37:42 -0700

    add blastn XML result test.

--- test/test2.js
+++ test/test2.js
@@ -1,28 +1,44 @@
-var appPath = require("app-root-path").path;
-var blast = require('blastjs');
+var assert = require("assert");
 var fs = require('fs-extra');
 
-blast.outputString(true); //optional, provides string output instead of JSON
 
-var dbPath = './dbs/example';
-var query = '>24.6jsd2.Tut\nGGTGTTGATCATGGCTCAGGACAAACGCTGGCGGCGTGCTTAATACATGCAAGTCGAACGGGCTACCTTCGGGTAGCTAGTG'
-+'\n>24.6jsd3.Tut\nATGATCATGGCTCAGATTGAACGCTGGCGGCATGCCTTACACATGCAAGTCGAACGGCAGCACGGGGAAGGGGCAACTCTTT';
 
-var options = {
-	query: query,
-	db: dbPath,
-	outfmt: 5,
-	outputDirectory: appPath+'/tmp'
-};
+describe('blastjs', function () {
+  this.timeout(15000);
+  describe('#blast', function () {
+    it('blastn XML result test', function(done){
+        this.timeout(90000); //Calling remote can take a little while.
+        //console.log('Querying remote database...');
+        var blast = require('../index.js');
+        var appPath = require("app-root-path").path;
+        //var blast = require('blastjs');
+        
+        blast.outputString(true); //optional, provides string output instead of JSON
+        
+        var dbPath = './test/example';
+        var query = '>24.6jsd2.Tut\nGGTGTTGATCATGGCTCAGGACAAACGCTGGCGGCGTGCTTAATACATGCAAGTCGAACGGGCTACCTTCGGGTAGCTAGTG'
+        +'\n>24.6jsd3.Tut\nATGATCATGGCTCAGATTGAACGCTGGCGGCATGCCTTACACATGCAAGTCGAACGGCAGCACGGGGAAGGGGCAACTCTTT';
 
-fs.ensureDir(options.outputDirectory);
-console.log("outputDirectory",options.outputDirectory);
+        var options = {
+                query: query,
+                db: dbPath,
+                outfmt: 5,
+                outputDirectory: appPath+'/tmp'
+        };
 
-blast.blast(options, function (err, output) {
-  if(!err){
-    console.log(output);
-  }
+        fs.ensureDir(options.outputDirectory);
+
+        var buffer = fs.readFileSync('./test/blastresult.xml');
+
+        blast.blast(options, function(err, results) {
+            assert.equal(err, null);
+            assert(results.match(buffer));     
+            done();
+        });
+    });
+  });
 });
+
 
 
 
