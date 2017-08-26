254f5760a7d102198dea9756dfb1d3412d500429
enuggetry <github@meyao.net>
2017-08-25 21:51:47 -0700

    narrow the check for existing blast installation.

--- util/blast_getBlast.js
+++ util/blast_getBlast.js
@@ -8,15 +8,17 @@
 var fs = require("fs-extra");
 var shelljs = require("shelljs");
 var finder = require('fs-finder');
-var thisPath = require("app-root-path").path;
+var thisPath = require("app-root-path").path + '/blastbin';
 
 var setupScript = "getBlast.js";
 
 // exit if already installed
-var found = finder.from(thisPath).findFiles('blastn');
-if (found.length > 0) {
-    console.log("NCBI Blast+ already installed in",thisPath);
-    process.exit(0);
+if (fs.pathExistsSync(thisPath)) {
+    var found = finder.from(thisPath).findFiles('blastn');
+    if (found.length > 0) {
+        console.log("NCBI Blast+ already installed in",thisPath);
+        process.exit(0);
+    }
 }
 // check if blastjs is a module
 if (fs.existsSync(thisPath+"/node_modules/blastjs/util/"+setupScript)) {
