cfa4a4c228dcdec8bcb9d957c69a8aab5a485fbf
enuggetry <github@meyao.net>
2017-12-22 02:37:54 -0800

    Fix download case of downloading entire blast database or partial database. Partial download allows for downloading a smaller dataset for quicker download for testing. The partial database will fix the NAL file upon fininshing.
    htgs.05 was tested because it was small, but known to yield some hits.
    However, other partial datasets should work.

--- util/blast_downloadDb.js
+++ util/blast_downloadDb.js
@@ -6,6 +6,7 @@ var path = require('path');
 var shell = require('shelljs');
 var approot = require('app-root-path');
 var fs = require("fs-extra");
+var async = require("async");
 
 // local options
 var options = [
@@ -44,30 +45,94 @@ if (typeof opt.argv[0] !== 'undefined') {
     });
 
     ftp.ls(blastDbPath+search+'*.tar.gz*', function(err, res) {
-        res.forEach(file => {
-            if (file.name.indexOf('.md5') === -1) {
-                
-                var blastDbPath = approot+'/blastdb/'+path.basename(file.name,'.tar.gz')+'/';
-                
-                fs.ensureDirSync(blastDbPath);
-                
-                var outPath = blastDbPath+path.basename(file.name);
-                
-                console.log('retriving',file.name,outPath);
-                
-                
-                ftp.get(file.name, outPath, function(err) {
-                    if (err) {
-                        console.log("failed to get",file.name,err);
-                        process.exit(1);
-                    }
-                    console.log('extracting',outPath);
-                    shell.exec(	'tar xvzf '+outPath+' -C '+blastDbPath);
-                    fs.unlink(outPath);                 // delete the tar file
-                    console.log('Blast DB now installed in',blastDbPath);
-                    process.exit(0);
-                });
+        
+        var filelist = [];
+ 
+        // eliminate files with .md5 extentions
+        for(var i in res) {
+            if (res[i].name.indexOf('.md5') === -1) filelist.push(res[i]);
+        }
+
+        // ensure target directory
+        var blastDbPath = approot+'/blastdb/'+search+'/';
+        fs.ensureDirSync(blastDbPath);
+        
+        // determine if this is a partial dataset (ie. "htgs.05" instead of "htgs")
+        var nalFile = false;
+        if (search.indexOf('.') !== -1) {
+            var sp = search.split('.');
+            if (sp[0].length && sp[1].length) nalFile = blastDbPath+sp[0]+'.nal';
+        }
+        
+        async.eachSeries(filelist, function(file, cb) {
+
+            console.log('Processing file ' + file.name);
+            download(file,blastDbPath, cb);
+
+        }, function(err) {
+            if( err ) {
+                console.log('A file failed to process');
+            } else {
+                console.log('All files have been processed successfully');
+              
+                if (nalFile) fixNalFile(nalFile,search);
             }
-        });
+        });        
+        
+        function download(file,blastDbPath,cb) {
+
+            //return cb();
+            var outPath = blastDbPath+path.basename(file.name);
+
+            console.log('retriving',file.name,outPath);
+            var totalSize = parseInt(file.size);
+            var totalSizeMb = (totalSize / 1000000.0).toFixed(2);
+
+            // create empty outPath file
+            fs.writeFileSync(outPath,"");
+
+            // setup interval timer to monitor progress.
+            var timer = setInterval(function() {
+                var stats = fs.statSync(outPath);
+                var sizeMb = (stats.size / 1000000.0).toFixed(2);
+                process.stdout.write('Downloading: '+sizeMb+'/'+totalSizeMb+"MB     \r");
+            },2000);
+
+            // do the ftp get operation
+            ftp.get(file.name, outPath, function(err) {
+                clearInterval(timer);
+                if (err) {
+                    console.log("failed to get",file.name,err);
+                    //process.exit(1);
+                    cb(err);
+                }
+                console.log('extracting',outPath);
+                shell.exec(	'tar xvzf '+outPath+' -C '+blastDbPath);
+                fs.unlink(outPath);                 // delete the tar file
+                console.log('Blast DB now installed in',blastDbPath);
+                //process.exit(0);
+                cb();
+            });
+        }
+        function fixNalFile(file,dbName) {
+            console.log('fixing .nal file',file,dbName);
+            
+            var content = fs.readFileSync(file,'utf-8');
+            
+            var lines = content.split("\n");
+            
+            for(var i in lines) {
+                if (lines[i].indexOf('DBLIST') !== -1) {
+                    lines[i] = 'DBLIST "'+dbName+'"';
+                }
+            }
+            var newContent = lines.join("\n");
+            
+            fs.writeFileSync(file,newContent+"\n");
+            //console.log(newContent);
+
+            
+        }
     });
 }
+
