4bc6d03e1494f48c60c56491e8d18c053493fda6
enuggetry <github@meyao.net>
2018-01-25 17:26:10 -0800

    postinstall now puts utils into the bin directory of approot.

--- util/postinstall.js
+++ util/postinstall.js
@@ -1,14 +1,24 @@
 var appPath = require("app-root-path").path;
 var shelljs = require("shelljs");
 var modPath = ""+shelljs.pwd();
+var fs = require('fs-extra');
 
 console.log("blastjs postinstall",modPath);
 
 // copy blast_* utils to the app directory.
 var src = modPath+"/util/blast_*";
-var trg = appPath;
+var trg = appPath+"/bin";
 
-//copy scripts to approot
-shelljs.cp(src,trg);
+try {
+    // if dir doesn't exist, create it
+    fs.ensureDirSync(trg);
+    
+    //copy scripts to approot
+    shelljs.cp(src,trg);
+}
+catch(err) {
+    console.log('failed to create',trg);
+    process.exit(1);
+}
 
-//console.log("blast postinstall done");
+console.log("blastjs postinstall done");
